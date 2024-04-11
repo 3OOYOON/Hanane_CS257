2983aabbb355e3c6dd0ea12c3a6348a425334806
3OOYOON <hanane.akeel@gmail.com>
2024-04-10 20:58:52 -0500

    queries for my table made in stearns

---
+++ SQL Lab Files/earthquakequery.sql
@@ -0,0 +1,6 @@
+select * from earthquakes where magnitude between 3 and 4;
+select * from earthquakes order by magtype;
+select * from earthquakes where magsource = 'us';
+select * from earthquakes where magsource = 'us' and magtype = 'mww';
+
+
