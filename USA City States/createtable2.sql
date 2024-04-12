8405e66ff15fd79409b610fb7170763f77ef04a7
Hanane H Akeel <145625433+3OOYOON@users.noreply.github.com>
2024-04-12 10:39:44 -0500

    another createtable for the state population data

---
+++ USA City States/createtable2.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS USA City State Population;
+CREATE TABLE USA City State Population (
+  city text,
+  state text,
+  population int,
+  longitude decimal,
+  latitude decimal
+);
