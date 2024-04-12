421b0fb4c27cd0ca196997497336a81e42178fe7
3OOYOON <hanane.akeel@gmail.com>
2024-04-12 10:34:46 -0500

    updated createtable1

--- USA City States/createtable1.sql
+++ USA City States/createtable1.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:USA City States/createtable.sql
 DROP TABLE IF EXISTS USA City State Population;
 CREATE TABLE USA City State Population (
   city text,
@@ -6,17 +5,4 @@ CREATE TABLE USA City State Population (
   population int,
   longitude decimal,
   latitude decimal
-=======
-DROP TABLE IF EXISTS earthquakes;
-CREATE TABLE earthquakes (
-  quakedate date,
-  quaketime time with time zone,
-  latitude real,
-  longitude real,
-  quakedepth real,
-  mag real,
-  id text,
-  place text,
-  quaketype text
->>>>>>> a34246e6e86b8e9e8e15351769f6855a706867e7:USA City States/createtable1.sql
 );
