01ad636bc88dde38fe0e5535f50012d508d5bfb0
Hanane H Akeel <145625433+3OOYOON@users.noreply.github.com>
2024-04-12 10:38:49 -0500

    put createtable2.sql in the wrong dirctory

--- SQL Lab Files/createtable2.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS USA City State Population;
-CREATE TABLE USA City State Population (
-  city text,
-  state text,
-  population int,
-  longitude decimal,
-  latitude decimal
-);
