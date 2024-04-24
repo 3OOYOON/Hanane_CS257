ab88065f401cd48c8d03d37ee762b289b80d595c
3OOYOON <hanane.akeel@gmail.com>
2024-04-24 10:43:16 -0500

    updated

--- Flask Lab/flask-test.py
+++ Flask Lab/flask-test.py
@@ -24,9 +24,9 @@ def my_display2(num1, num2):
     float(num2)
 
     if num1 and num2 == float:
-    return '<h1 style="color:rgb(11, 11, 97)">' + (num1 + num2) + '</h1>'
+        return '<h1 style="color:rgb(11, 11, 97)">' + (num1 + num2) + '</h1>'
     else:
-    return '<h1 style="color:rgb(11, 11, 97)">' + "at least one of your inputs is not a number" + '</h1>'
+        return '<h1 style="color:rgb(11, 11, 97)">' + "at least one of your inputs is not a number" + '</h1>'
 
 if __name__ == '__main__':
     my_port = 5209
