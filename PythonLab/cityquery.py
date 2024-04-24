c007e571f6afc7ea07a81809d9371af3fae1ee55
3OOYOON <hanane.akeel@gmail.com>
2024-04-23 21:31:51 -0500

    updated

--- PythonLab/cityquery.py
+++ PythonLab/cityquery.py
@@ -47,7 +47,7 @@ def query_db():
     # Total population by state input
     state_name = input("Enter a state (abbreviation or full name): ")
     if len(state_name) == 2:
-        cur.execute("select sum(population) from usa_city_state_population where state in (select name from states WHERE code = %s);", (state_name,))
+        cur.execute("select sum(population) from usa_city_state_population where state in (select name from states where code = %s);", (state_name,))
     else:
         cur.execute("select sum(population) from us_state_pop where state = %s;", (state_name,))
     total_pop = cur.fetchone()[0]
@@ -56,4 +56,4 @@ def query_db():
     cur.close()
     conn.close()
 
-
+    query_db()
