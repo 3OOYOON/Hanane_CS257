ce8c3f4a078da8e298fa4c5ac1672fd3aa6c773d
3OOYOON <hanane.akeel@gmail.com>
2024-04-24 20:13:56 -0500

    updated

--- Flask Lab/flask-test.py
+++ Flask Lab/flask-test.py
@@ -22,7 +22,7 @@ def my_display2(num1, num2):
     int(num2)
     sum = num1 + num2;
 
-    if type(num1)==int and type(num2) == int:
+    if isinstance(num1, int) and isinstance(num2, int):
         return '<h1 style="color:rgb(11, 11, 97)">' + sum + '</h1>'
     else:
         return '<h1 style="color:rgb(11, 11, 97)">' + "at least one of your inputs is not a number" + '</h1>'
