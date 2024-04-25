74f0947f958a337db0de79356105f984db5a29e5
3OOYOON <hanane.akeel@gmail.com>
2024-04-24 20:38:04 -0500

    updated

--- Flask Lab/flask-test.py
+++ Flask Lab/flask-test.py
@@ -18,15 +18,14 @@ def my_color(word1):
 
 @app.route('/add/<num1>/<num2>')
 def my_display2(num1, num2):
-    int(num1)
-    int(num2)
+    num1 = int(num1)
+    num2 = int(num2)
     sum = num1 + num2;
 
     if isinstance(num1, int) and isinstance(num2, int):
         return '<h1 style="color:rgb(11, 11, 97)">' + sum + '</h1>'
     else:
-        print(type(num1))
-        return '<h1 style="color:rgb(11, 11, 97)">' + "at least one of your inputs is not a number " +num1 + "- - " + num2 + str(type(num1)) + str(type(num2)) +'</h1>'
+        return '<h1 style="color:rgb(11, 11, 97)">' + "at least one of your inputs is not a number " +'</h1>'
     
 
 
