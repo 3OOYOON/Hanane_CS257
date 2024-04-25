f26357ff4fc116f693db14e25c54f8cea38f09d8
3OOYOON <hanane.akeel@gmail.com>
2024-04-24 20:26:00 -0500

    updated

--- Flask Lab/flask-test.py
+++ Flask Lab/flask-test.py
@@ -25,8 +25,12 @@ def my_display2(num1, num2):
     if isinstance(num1, int) and isinstance(num2, int):
         return '<h1 style="color:rgb(11, 11, 97)">' + sum + '</h1>'
     else:
-        return '<h1 style="color:rgb(11, 11, 97)">' + "at least one of your inputs is not a number " +num1 + "- - " + num2 + '</h1>'
+        return '<h1 style="color:rgb(11, 11, 97)">' + "at least one of your inputs is not a number " +num1 + "- - " + num2 + type(num1) + type(num2)'</h1>'
+
+    
+
 
 if __name__ == '__main__':
+
     my_port = 5209
     app.run(host='0.0.0.0', port = my_port)
