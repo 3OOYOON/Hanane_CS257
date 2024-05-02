75982b950f90b27d516d3e137f10920983ed760c
3OOYOON <hanane.akeel@gmail.com>
2024-05-02 00:47:15 -0500

    updated

--- Flask Lab/task3-of-flasklab-part2.py
+++ Flask Lab/task3-of-flasklab-part2.py
@@ -6,18 +6,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return render_template("index.html")
+    return render_template("index2.html")
 
-@app.route('/rand/<low>/<high>')
+@app.route('/rand/image/gen')
 def rand(low, high):
     #Input values that come from a URL (i.e., @app.route)
     #   are always strings so I need to convert the type to int
     low_int = int(low)
     high_int = int(high)
-
+    
     num = random.randint(low_int, high_int)
     return render_template("random.html", randNum = num)
 
 if __name__ == '__main__':
     my_port = 5209
-    app.run(host='0.0.0.0', port = my_port)
+    app.run(host='0.0.0.0', port = my_port)  
