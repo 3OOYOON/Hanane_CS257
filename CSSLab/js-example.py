5af23147ad7a4c717d636149aceed6dcb1fa26db
3OOYOON <hanane.akeel@gmail.com>
2024-05-16 09:23:54 -0500

    updated

--- CSSLab/js-example.py
+++ CSSLab/js-example.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
 @app.route('/rps')
 def welcome():
     message = """ Welcome to my Rock Paper Scissors Webpage.
-    As you can guess you can play rock paper scissors on this site
-    Either press rock, paper, or scissors and see if you can beat the computer
-    You may either win, lose, or get in a tie"""
+    As you can guess you can play rock paper scissors on this site.
+    Either press rock, paper, or scissors and see if you can beat the computer.
+    You may either win, lose, or get in a tie."""
     return render_template("homepage.html", someText = message)
 
 
