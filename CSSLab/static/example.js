bb2f8353e1d801c3d6717574794663107973362d
3OOYOON <hanane.akeel@gmail.com>
2024-05-16 09:12:34 -0500

    updated

--- CSSLab/static/example.js
+++ CSSLab/static/example.js
@@ -1,14 +1,7 @@
-function play_game() {
-  var form = document.getElementById("gameForm");
-  form.addEventListener("click", function(event) {
-    var user_choice = event.target.value;
-    if (user_choice) {
-      var result = fight(user_choice);
-      document.getElementById("result").innerText = result;
-    }
-  });
+function play_game(user_choice) {
+  var result = fight(user_choice);
+  document.getElementById("result").innerText = result;
 }
-
 
 function fight(user_choice) {
   var computer_choice = Math.floor(Math.random() * 3);
@@ -16,15 +9,15 @@ function fight(user_choice) {
 
   // 0 is rock, 1 is paper, 2 is scissors
   if (
-    (computer_choice === 0 && (user_choice === "p" || user_choice === "P")) ||
-    (computer_choice === 1 && (user_choice === "s" || user_choice === "S")) ||
-    (computer_choice === 2 && (user_choice === "r" || user_choice === "R"))
+    (computer_choice === 0 && user_choice === "p") ||
+    (computer_choice === 1 && user_choice === "s") ||
+    (computer_choice === 2 && user_choice === "r")
   ) {
     result = "You win!";
   } else if (
-    (computer_choice === 0 && (user_choice === "r" || user_choice === "R")) ||
-    (computer_choice === 1 && (user_choice === "p" || user_choice === "P")) ||
-    (computer_choice === 2 && (user_choice === "s" || user_choice === "S"))
+    (computer_choice === 0 && user_choice === "r") ||
+    (computer_choice === 1 && user_choice === "p") ||
+    (computer_choice === 2 && user_choice === "s")
   ) {
     result = "It's a tie!";
   } else {
